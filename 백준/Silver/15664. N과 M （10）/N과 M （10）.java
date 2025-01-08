32fbd993319dd0eb41ac5d0caf2eb94bde066866
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-01-08 11:40:30 +0900

    [Silver II] Title: N과 M (10), Time: 172 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean[] isChecked;
+    static int[] answerArray;
+    static int[] inputArray;
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] split = scanner.nextLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
+
+        inputArray = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        isChecked = new boolean[N];
+        answerArray = new int[M];
+        recursive(0, 0);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    static void recursive(int start, int depth) {
+        if (depth == M) {
+            for (int i : answerArray) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = start; i < N; i++) {
+            if (!isChecked[i] && inputArray[i] != prev) {
+                isChecked[i] = true;
+                answerArray[depth] = inputArray[i];
+                prev = inputArray[i];
+                recursive(i+1, depth+1);
+                isChecked[i] = false;
+            }
+        }
+    }
+}
