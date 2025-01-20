39d9ce864c90eb7de898707b3c948aaa3af00e85
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-01-20 16:18:49 +0900

    [level 3] Title: 양과 늑대, Time: 5.95 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
 
 ### 성능 요약
 
-메모리: 91 MB, 시간: 4.35 ms
+메모리: 99.2 MB, 시간: 5.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 17일 18:08:40
+2025년 01월 20일 16:18:47
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,61 +1,68 @@
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-class Solution {
 
-        private static int answer;
-        private static final List<List<Integer>> GRAPH = new ArrayList<>();
 
-        public int solution(int[] info, int[][] edges) {
+ class Solution {
 
-            // 1. 초기 세팅
+        // [1,8], [2,4]
+        static final List<List<Integer>> childNodeList = new ArrayList<>();
+        static int answer = 0;
+
+     int solution(int[] info, int[][] edges) {
+
+            // 노드의 수만큼 ArrayList 생성
             for (int i = 0; i < info.length; i++) {
-                GRAPH.add(i, new ArrayList<>());
+                childNodeList.add(new ArrayList<>());
             }
 
-            for (int[] edge : edges) {
-                GRAPH.get(edge[0]).add(edge[1]);
+            // 자식노드끼리 모으기, 부모가 누구인지는 info 배열로 접근.
+            for (int i = 0; i < edges.length; i++) {
+                childNodeList.get(edges[i][0]).add(edges[i][1]);
             }
 
-            /**
-             *  dfs(info[] node, 0, 빈 리스트 0, 0 )
-             *
-             */
-            ArrayList<Integer> list = new ArrayList<>();
-            list.add(0); // 루트 노드 추가
+            // 상태가 필요하다. 참조로 넘기면 안된다. 독립적인 상태 필요.
+            ArrayList<Integer> currentCandidate = new ArrayList<>();
+            currentCandidate.add(0); // 루트 노드 넣기.
 
-            dfs(info, list, 0, 0, 0);
+            dfs(info, currentCandidate, 0, 0, 0);
+
             return answer;
         }
 
-        public void dfs(int[] info, ArrayList<Integer> list, int node, int sheep, int wolf) {
+        private static void dfs(int[] info, ArrayList<Integer> currentCandidate, int node, int sheep, int wolf) {
             if (info[node] == 0) {
-                sheep++;
+                // 앙이면?
+                sheep += 1;
             } else {
-                wolf++;
+                // 늑대면?
+                wolf += 1;
             }
 
+            // 잡아먹히면 함수 종료
             if (sheep <= wolf) {
                 return;
             }
 
+            // 잡아먹히지 않은 경우, answer 갱신
             answer = Math.max(answer, sheep);
 
+            // 현재 리스트 가져옴
+            ArrayList<Integer> tmpCandidate = new ArrayList<>(currentCandidate);
 
-            // 리스트 복사
-            ArrayList<Integer> next = new ArrayList<>(list);
-
-            // 자신의 자식 노드들 넣기.
-            if (!GRAPH.get(node).isEmpty()) {
-                // 자식이 있는 경우
-                next.addAll(GRAPH.get(node));
+            // 자식 정보 추가
+            if (!childNodeList.get(node).isEmpty()) {
+                tmpCandidate.addAll(childNodeList.get(node));
             }
 
-            // 자기는 뺀다.
-            next.remove(Integer.valueOf(node));
+            // 자기 자신은 뺀다
+            tmpCandidate.remove(Integer.valueOf(node)); // int 를 집어넣으면 해당 인덱스가 제거됨.
 
-            for (int n : next) {
-                dfs(info, next, n, sheep, wolf);
+            // 상태를 공유하는 노드 재귀 호출.
+            for (int n : tmpCandidate) {
+                dfs(info, tmpCandidate, n, sheep, wolf);
             }
         }
-}
+    }
