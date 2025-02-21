bac1629fd40f1903dab86adcc2b6772451c4976e
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-02-21 13:39:15 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,25 +1,6 @@
-import java.util.HashMap;
 class Solution {
-        String solution(String[] id_pwd, String[][] db) {
-
-
-            String id = id_pwd[0];
-            String pwd = id_pwd[1];
-
-            HashMap<String, String> map = new HashMap<>();
-
-            for (int i = 0; i < db.length; i++) {
-                map.put(db[i][0], db[i][1]);
-            }
-
-            if (map.containsKey(id)) {
-                if (map.get(id).equals(pwd)) {
-                    return "login";
-                } else {
-                    return "wrong pw";
-                }
-            } else {
-                return "fail";
-            }
-        }
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        return answer;
     }
+}
