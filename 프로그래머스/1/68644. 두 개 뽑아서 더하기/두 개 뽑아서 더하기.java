5fdd378cd9e326b759e1d449bf6fe6f26e90be86
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-04-16 13:17:35 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 22.90 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,26 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet; 
+class Solution {
+     
+     	static HashSet<Integer> set = new HashSet<>();
 
-import java.util.*;
 
-class Solution{
-        public int[] solution(int[] numbers) {
-            HashSet<Integer> set = new HashSet<>();
+		public int[] solution(int[] numbers) {
 
-            for (int i = 0; i < numbers.length; i++) {
-                for (int j = i+1; j < numbers.length; j++) {
-                    set.add(numbers[i] + numbers[j]);
-                }
-            }
+			// 시작점 있어야 함
+			// 2 1 3 4 1
+			Arrays.sort(numbers);
 
-          //  System.out.println(set);
+			// numbers 넘겨야 하고, 시작점 넘겨야 하고, 빈 리스트 넘겨야 하고
+			recursive(0, new ArrayList<>(), numbers);
+			
+int[] array = set.stream().mapToInt(Integer::intValue).toArray();
+			Arrays.sort(array);
+			return array;		}
 
-            List<Integer> tempSet = new ArrayList<>(set);
-            Collections.sort(tempSet);
+		private static void recursive(int startIndex, ArrayList<Integer> arrayList, int[] numbers) {
 
-            int[] result = new int[tempSet.size()];
-            for (int i = 0; i < result.length; i++) {
-                result[i] = tempSet.get(i);
-            }
-            System.out.println(Arrays.toString(result));
-            return result;
-        }
-    }
+			// 탈출 조건
+			// 리스트 길이가 2면 탈출
+			if (arrayList.size() == 2) {
+				int sum = arrayList.stream().reduce((a, b) -> a + b).get();
+				set.add(sum);
+				return;
+			}
+
+			for (int i = startIndex; i < numbers.length; i++) {
+
+				int number = numbers[i];
+				arrayList.add(number);
+				recursive(i + 1, arrayList, numbers);
+				arrayList.remove(arrayList.size() - 1);
+			}
+
+		}
+
+	}
