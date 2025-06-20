b49a37acc669a37b98f2c649141ce9af2b151956
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-20 12:53:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 17112 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,69 +1,90 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
-class Main {
-	public static void main(String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String input = br.readLine();
+        int N = Integer.parseInt(input.split(" ")[0]);
+        int K = Integer.parseInt(input.split(" ")[1]);
+        
+        // 방문 여부 체크
+        int[] visited = new int[100001];
+        visited[N] = 0;
+        
+        // queue
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.offer(N);
+        
+        while (!queue.isEmpty()) {
+          int now = queue.poll(); // 5
+          int current = visited[now]; // 0
+          int next = current + 1; // 1
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String input = br.readLine();
-		String[] split = input.split(" ");
-		int N = Integer.parseInt(split[0]);
-		int K = Integer.parseInt(split[1]);
-
-		// N - 수빈이의 위치
-		// K - 동생의 위치
-
-		// 걷거나 순간이동, -1, 1, *2
-		// 중복되면 안됨.
-
-		int[] visited = new int[100001];
-		for (int i = 0; i < visited.length; i++) {
-			visited[i] = -1;
-		}
-
-		int sec = 0;
-		ArrayDeque<Integer> queue = new ArrayDeque<>();
-
-		// 수빈이의 위치는 0초일때
-		visited[N] = sec;
-
-		// 수빈이의 위치를 큐에 넣음
-		queue.offer(N);
-
-		while (!queue.isEmpty()) {
-			Integer location = queue.poll();
-			int now = visited[location];
-
-			// 동생의 위치인 경우
-			if (location == K) {
-				System.out.println(now);
-				return;
-			}
-
-			// 동생의 위치가 아닌 경우
-			// 배열로 빼서, 배열의 요소들이 범위 내에 드는지 화인해보기
-			int[] next = new int[] {location - 1, location + 1, location * 2};
-
-			for (int n : next) {
-				// -1이여야하고, 범위 내에 들어아 함
-				if ( n >= 0 && n < visited.length && visited[n] == -1) {
-					int nextTime = now + 1;
-					visited[n] = nextTime;
-					if (n == K) {
-						System.out.println(nextTime);
-						return;
-					}
-					queue.offer(n);
-				}
-			}
-
-		}
-
-	}
+          if (now == K) {
+              bw.write(String.valueOf(current));
+              bw.flush();
+              bw.close();
+              br.close();
+              return;
+          }
+           
+          // 앞으로 한칸
+          int forward = now + 1;
+        
+          // 뒤로 한칸
+          int backward = now - 1;
+        
+          // x2 순간이동
+          int teleport = now * 2;
+        
+          // 맞는지 확인
+          if (forward == K || backward == K || teleport == K) {
+              bw.write(String.valueOf(next));
+              bw.flush();
+              bw.close();
+              br.close();
+              return;
+          }
+            
+        // 조건 확인 0 100 000
+        if (forward >= 0 && forward <= 100000 && visited[forward] == 0){
+            if (forward + 1 == K || forward - 1 == K || forward * 2 == K) {
+                bw.write(String.valueOf(next + 1));
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+            visited[forward] = next;
+            queue.offer(forward);
+        }
+            
+        if (backward >= 0 && backward <= 100000 && visited[backward] == 0){
+            if (backward + 1 == K || backward - 1 == K || backward * 2 == K) {
+                bw.write(String.valueOf(next + 1));
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+            visited[backward] = next;
+            queue.offer(backward);
+        }
+            
+        if (teleport >= 0 && teleport <= 100000 && visited[teleport] == 0){
+            if (teleport + 1 == K || teleport - 1 == K || teleport * 2 == K) {
+                bw.write(String.valueOf(next + 1));
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+              visited[teleport] = next;
+              queue.offer(teleport);
+            }
+        }
+    }
 }
