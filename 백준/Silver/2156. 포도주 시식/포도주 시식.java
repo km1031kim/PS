0e7133f1159537f1d8e3e9971438bfb24e6fff3c
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-21 14:57:33 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 15016 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,47 +1,66 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
+		/**
+		 6
+		 6
+		 10
+		 13
+		 9
+		 8
+		 1
+		 **/
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int count = Integer.parseInt(br.readLine());
 
-		int[] wineArray = new int[count];
-		for (int i = 0; i < wineArray.length; i++) {
-			wineArray[i] = Integer.parseInt(br.readLine());
-		}
-
-		// DP 배열을 만든다.
+		int[] wine = new int[count];
 		int[] dp = new int[count];
 
-		dp[0] = wineArray[0];
-
-		if (count > 1) {
-			dp[1] = wineArray[0] + wineArray[1];
+		for (int i = 0; i < wine.length; i++) {
+			int weight = Integer.parseInt(br.readLine());
+			wine[i] = weight;
 		}
 
-		if (count > 2) {
-			dp[2] = Math.max(dp[1],
-				Math.max(dp[0] + wineArray[2], wineArray[1] + wineArray[2]));
+		// count == 1
+		dp[0] = wine[0];
+
+		if (count == 1) {
+			bw.write(String.valueOf(dp[count-1]));
+			bw.flush();
+			return;
 		}
 
+		// count == 2
+
+		dp[1] = wine[0] + wine[1];
+		if (count == 2) {
+			bw.write(String.valueOf(dp[count-1]));
+			bw.flush();
+			return;
+		}
+		dp[2] = Math.max(Math.max(wine[0] + wine[1], wine[0] + wine[2]), wine[1] + wine[2]);
+		if (count == 3) {
+			bw.write(String.valueOf(dp[count-1]));
+			bw.flush();
+			return;
+		}
+		// count > 3
 		if (count > 3) {
-			for (int i = 3; i < wineArray.length; i++) {
-				dp[i] = Math.max(dp[i - 1],
-					Math.max(
-						dp[i - 2] + wineArray[i],
-						dp[i - 3] + wineArray[i] + wineArray[i - 1]
-					));
-
+			for(int i = 3; i < count; i++) {
+				dp[i] = Math.max(
+					// 세번째라 못먹음
+					Math.max(dp[i-1],
+						dp[i-3] + wine[i-1] + wine[i]),
+					dp[i-2] + wine[i]
+				);
 			}
 		}
 
-		bw.write(String.valueOf(dp[count - 1]));
+		bw.write(String.valueOf(dp[count-1]));
 		bw.flush();
 
 		bw.close();
