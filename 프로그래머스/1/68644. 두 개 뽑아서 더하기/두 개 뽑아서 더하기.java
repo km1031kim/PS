6cbae462e404c3aaabb39ff68c5f3a6ea60db233
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-24 17:19:07 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 24.27 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,26 +1,53 @@
-
 import java.util.*;
+import java.io.*;
 
-class Solution{
-        public int[] solution(int[] numbers) {
-            HashSet<Integer> set = new HashSet<>();
-
-            for (int i = 0; i < numbers.length; i++) {
-                for (int j = i+1; j < numbers.length; j++) {
-                    set.add(numbers[i] + numbers[j]);
-                }
+class Solution {
+    
+    HashSet<Integer> set = new HashSet<>();
+    
+    public int[] solution(int[] numbers) {
+        
+        // 재귀네
+        // set 필요
+        // set -> array -> sort
+        
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        
+        for (int i = 0; i < numbers.length - 1; i++) {
+           recursive(i, numbers, arrayList);
+        }
+        
+        int[] answer = set.stream()
+            .mapToInt(Integer::intValue).toArray();
+        
+        Arrays.sort(answer);
+        
+      
+        return answer;
+    }
+    
+    public void recursive(int startIndex, int[] numbers, ArrayList<Integer> arrayList) {
+        // 탈출 조건
+        if (arrayList.size() == 2) {
+            
+            int sum = 0;
+            for (int component : arrayList) {
+                sum += component;
             }
-
-          //  System.out.println(set);
-
-            List<Integer> tempSet = new ArrayList<>(set);
-            Collections.sort(tempSet);
-
-            int[] result = new int[tempSet.size()];
-            for (int i = 0; i < result.length; i++) {
-                result[i] = tempSet.get(i);
-            }
-            System.out.println(Arrays.toString(result));
-            return result;
+            
+            set.add(sum);
+            return;
+        }
+        
+        for (int i = startIndex; i < numbers.length; i++) {
+            int tmp = numbers[i];
+            arrayList.add(tmp);
+            recursive(i+1, numbers, arrayList);
+            
+            // 탈출 후 제거
+            int last = arrayList.size() - 1;
+            arrayList.remove(last);
+            
         }
     }
+}
