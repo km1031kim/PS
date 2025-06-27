668bacfb06973a0823e06fc0396f3df3220977e7
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-27 15:12:49 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.10 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,32 +1,24 @@
-import java.util.Arrays;
- 
+import java.util.*;
+
 class Solution {
-        public int[][] solution(int[][] arr1, int[][] arr2) {
-
-            // 기준 배열과 같은 크기를 가짐.
-            int[][] answer = new int[arr1.length][arr2[0].length];
-
-            for (int i = 0; i < arr1.length; i++) {
-                // 행 반복
-              //  System.out.println("i = " + i);
-                for (int j = 0; j < arr2[0].length; j++) {
-                 //  System.out.println("j = " + j);
-                    int sum = 0;
-                    for (int k = 0; k < arr1[0].length; k++) {
-                      //  System.out.println("i = " + i + ", j = " + j + ", k = " + k);
-
-                        sum += arr1[i][k] * arr2[k][j];
-                        // 00 00
-                        // 01 10
-                        // 02 20
-
-                        // j ==1
-
-                    }
-                 //   System.out.println("sum = " + sum);
-                    answer[i][j] = sum;
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        
+        for (int i = 0; i < arr1.length; i++){
+            // 1, 4
+            
+            for (int j = 0; j < arr2[0].length; j++) {
+                
+                int sum = 0;
+                // 0 0 * 0 0 +  0 1 * 1 0
+                for (int k = 0; k < arr1[i].length; k++) {
+                    sum += arr1[i][k] * arr2[k][j];
                 }
+                
+                answer[i][j] = sum;
             }
-            return answer;
         }
+        return answer;
     }
+}
