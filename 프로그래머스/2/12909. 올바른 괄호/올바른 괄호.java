95280008ce7f12dfb456264e5edce253f537be92
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-29 15:13:32 +0900

    [level 2] Title: 올바른 괄호, Time: 21.80 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,33 +1,38 @@
+import java.util.*;
 
-import java.util.ArrayDeque;
- 
 class Solution {
-
-		public boolean solution(String s) {
-
-			ArrayDeque<Character> stack = new ArrayDeque<>();
-
-			for (int i = 0; i < s.length(); i++) {
-				char c = s.charAt(i);
-
-				if (stack.isEmpty()) {
-					stack.push(c);
-					continue;
-				}
-
-				// 스택에 요소가 존재하는 경우 -> 비교해야함
-
-				if (c == ')') {
-					Character top = stack.peek();
-					if (top != c) {
-						stack.pop();
-					}
-					continue;
-				}
-
-				stack.push(c);
-			}
-
-			return stack.isEmpty();
-		}
-	}
+    boolean solution(String s) {
+        
+        char[] chars = s.toCharArray();
+        
+        if (chars[0] == ')') {
+            return false;
+        }
+        
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < chars.length; i++) {
+            
+            char input = chars[i];
+            
+            if (stack.isEmpty()) {
+                stack.push(input);
+                continue;
+            }
+            
+            // 비어있지 않다. 맨 위꺼 확인
+            char top = stack.peek();
+            if (top == '(' && input == ')') {
+                stack.pop();
+                continue;
+            }
+            
+            stack.push(top);
+        }
+        
+        if (stack.isEmpty()) {
+            return true;
+        }
+        
+        return false;
+    }
+}
