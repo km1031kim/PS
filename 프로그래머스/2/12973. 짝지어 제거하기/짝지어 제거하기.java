21857fd3db3b680aab8b7bf9b8ea5d6885bf83ea
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-29 17:12:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.78 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,36 +1,31 @@
+import java.util.*;
 
-import java.util.ArrayDeque;
- 
+class Solution
+{
+    public int solution(String s)
+    {
+        
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);    
+            
+            if (stack.isEmpty()) {
+                stack.push(current);
+                continue;
+            }
+            
+            // 비어있지 않은 경우
+            char top = stack.peek();
+            if (current == top) {
+                stack.pop();
+                continue;
+            }
+            
+            stack.push(current);
+            
+        }
 
-class Solution {
-
-	public int solution(String s) {
-
-			ArrayDeque<String> stack = new ArrayDeque<>();
-
-			String[] split = s.split("");
-
-
-			for (int i = 0; i < s.length(); i++) {
-				String word = split[i];
-
-				if (stack.isEmpty()) {
-					stack.push(word);
-					continue;
-				}
-
-				// 비어있지 않다면?
-				String top = stack.peek();
-
-				if (word.equals(top)) {
-					stack.pop();
-					continue;
-				}
-
-				stack.push(word);
-			}
-
-			return stack.isEmpty() ? 1 : 0;
-		}
-
-	}
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
