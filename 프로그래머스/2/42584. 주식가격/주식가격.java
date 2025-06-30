7b5727e3e7fcf7a93929eb3acda7564be942ae6b
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-06-30 13:54:21 +0900

    [level 2] Title: 주식가격, Time: 22.39 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,35 +1,26 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int[] answer = new int[prices.length];
 
-import java.util.ArrayDeque;
+        
+        for (int i = 0; i < prices.length; i++) {
+            int now = prices[i];
+            
+            for (int j = i + 1; j < prices.length; j++) {
+                int next = prices[j]; // 2
+                
+                answer[i] = answer[i] + 1;  
 
-
-   class Solution{
-        public int[] solution(int[] prices) {
-            ArrayDeque<Integer> stack = new ArrayDeque<>();
-            int[] answer = new int[prices.length];
-
-            for (int i = 0; i < prices.length; i++) {
-
-                // i가 push 된다.
-                if (i == 0) {
-                    stack.push(i);
+                if (next < now) {
+                    break;
                 }
-
-                if (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                    // 주식이 내려간 경우.
-                    while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                        int time = i - stack.peek(); //
-                        answer[stack.peek()] = time;
-                        stack.pop();
-                    }
-                    stack.push(i);
-                } else {
-                    stack.push(i);
-                }
+                
             }
-            for (Integer i : stack) {
-                answer[i] = prices.length - i - 1;
-            }
-            return answer;
+            
+            // System.out.println(i + " : " + answer[i]);
         }
+        
+        return answer;
     }
-
+}
