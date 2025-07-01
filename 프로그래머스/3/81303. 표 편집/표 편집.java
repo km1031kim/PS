9b03e6ececf2a95f3b996afc5fceebed4d72d2b2
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-07-01 14:51:06 +0900

    [level 3] Title: 표 편집, Time: 399.60 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,114 +1,60 @@
+import java.util.*;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-
-import static java.lang.Character.getNumericValue;
-import static java.lang.Character.isDefined;
-
-
- class Solution {
-        public String solution(int n, int k, String[] cmd) {
-
-            // ArrayList 세팅.
-            Node[] nodeArray = initNodeArray(n);
-
-            // C 커맨드를 통해 제거된 노드는 스택에 들어갑니다.
-            ArrayDeque<Node> deleteStack = new ArrayDeque<>();
-
-            Node nowNode = nodeArray[k];
-
-
-            for (int i = 0; i < cmd.length; i++) {
-                String command = cmd[i];
-                // 업다운의 경우 그냥 해당 수만큼 prev, next로 이동하면 됨..
-                // 제거하거나 추가할 때는 그 위치를 정해줄거기 때문에 별도로 카운팅을 하지 않아도 된다.
-                if (command.startsWith("D")) {
-                  //  int distance = getNumericValue(command.charAt(command.length() - 1));
-                    Integer distance = Integer.valueOf(command.substring(2));
-                    for (int j = 0; j < distance; j++) {
-                        nowNode = nowNode.next;
-                    }
-                    continue;
-                } else if (command.startsWith("U")) {
-                  //  int distance = getNumericValue(command.charAt(command.length() - 1));
-                    Integer distance = Integer.valueOf(command.substring(2));
-                    for (int j = 0; j < distance; j++) {
-                        nowNode = nowNode.prev;
-                    }
-                    continue;
-                } else  if (command.equals("C")) {
-                    // 배열에서 빼는 경우 스택에 추가.
-                    deleteStack.push(nowNode);
-                    if (nowNode.prev == null) {
-                        // 첫번째 노드인 경우
-                        nowNode.next.prev = null;
-                        nowNode = nowNode.next;
-                    } else if (nowNode.next == null) {
-                        // 마지막 노드인 경우
-                        nowNode.prev.next = null;
-                        nowNode = nowNode.prev;
-                    } else {
-                        // 중간 노드들, 앞뒤로 주소 바꾸기
-                        nowNode.prev.next = nowNode.next;
-                        nowNode.next.prev = nowNode.prev;
-                        nowNode = nowNode.next;
-                    }
-                } else if (command.equals("Z")) {
-                    Node retrivedNode = deleteStack.pop();
-
-                    if (retrivedNode.next == null) {
-                        // 마지막 노드였던 경우
-                        retrivedNode.prev.next = retrivedNode;
-                    } else if (retrivedNode.prev == null) {
-                        // 첫번째 노드였던 경우
-                        retrivedNode.next.prev = retrivedNode;
-                    } else {
-                        retrivedNode.next.prev = retrivedNode;
-                        retrivedNode.prev.next = retrivedNode;
-                    }
-                }
-            }
-
-            String[] answer = new String[n];
-            Arrays.fill(answer, "O");
-            for (Node node : deleteStack) {
-                answer[node.number] = "X";
-            }
-
-            return String.join("",answer);
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+        Node[] nodeList = new Node[n];
+        for (int i = 0; i < n; i++) {
+            nodeList[i] = new Node(i);
         }
 
-        private static Node[] initNodeArray(int n) {
-            // 노드 생성 후 ArrayList에 넣기.
-            // 인덱스에 넣다뺏다 해야하니까 ArrayList로..
-            Node[] nodeArray = new Node[n];
-            for (int i = 0; i < n; i++) {
-                nodeArray[i] = new Node(i);
-            }
-
-            // 주소 세팅
-            for (int i = 0; i < n; i++) {
-                if (i > 0) {
-                    nodeArray[i].prev = nodeArray[i - 1];
-                }
-                if (i < n - 1) {
-                    nodeArray[i].next = nodeArray[i + 1];
-                }
-            }
-            return nodeArray;
+        for (int i = 0; i < n; i++) {
+            if (i > 0) nodeList[i].prev = nodeList[i - 1];
+            if (i < n - 1) nodeList[i].next = nodeList[i + 1];
         }
-        static class Node {
-            Node prev;
-            int number; // 디버깅을 위한 수
-            Node next;
 
-            public Node(int number) {
-                this.number = number;
+        Node currentNode = nodeList[k];
+        Deque<Node> deleteStack = new ArrayDeque<>();
+        Set<Integer> deleted = new HashSet<>();
+
+        for (String c : cmd) {
+            String[] split = c.split(" ");
+            String command = split[0];
+
+            if (command.equals("U")) {
+                int x = Integer.parseInt(split[1]);
+                for (int i = 0; i < x; i++) currentNode = currentNode.prev;
+            } else if (command.equals("D")) {
+                int x = Integer.parseInt(split[1]);
+                for (int i = 0; i < x; i++) currentNode = currentNode.next;
+            } else if (command.equals("C")) {
+                deleteStack.push(currentNode);
+                deleted.add(currentNode.index);
+
+                if (currentNode.prev != null) currentNode.prev.next = currentNode.next;
+                if (currentNode.next != null) currentNode.next.prev = currentNode.prev;
+                currentNode = (currentNode.next != null) ? currentNode.next : currentNode.prev;
+            } else if (command.equals("Z")) {
+                Node restored = deleteStack.pop();
+                deleted.remove(restored.index);
+
+                if (restored.prev != null) restored.prev.next = restored;
+                if (restored.next != null) restored.next.prev = restored;
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(deleted.contains(i) ? 'X' : 'O');
+        }
+
+        return sb.toString();
     }
 
-
-
+    class Node {
+        int index;
+        Node prev, next;
+        Node(int index) {
+            this.index = index;
+        }
+    }
+}
