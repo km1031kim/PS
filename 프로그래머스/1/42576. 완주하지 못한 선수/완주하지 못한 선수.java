2805facc4748db53f41a5fdcb622e33ec8dcdf52
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-07-02 10:10:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.75 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,41 +1,39 @@
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-
- class Solution {
-        public String solution(String[] participant, String[] completion) {
-
-
-            HashMap<String, Integer> map = new HashMap<>();
-
-            for (String s : participant) {
-                if (map.containsKey(s)) {
-                    // 이미 있으면? 1 추가
-                    Integer i = map.get(s);
-                    map.put(s, ++i);
-                } else {
-                    map.put(s, 1);
-                }
-            }
-
-           // System.out.println(map);
-
-
-            for (String s : completion) {
-                if (map.containsKey(s)) {
-                    Integer i = map.get(s);
-                    map.put(s, --i);
-                }
-                if (map.get(s) == 0) {
-                    map.remove(s);
-                }
-            }
-
-            String answer = "";
-            for (String s : map.keySet()) {
-               answer = s;
-            }
-            return answer;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+                
+        // 10만명.. 속도가 중요하다..
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < participant.length; i++) {
+            // 있으면 1 추가
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
+        
+
+        
+        for (int i = 0; i < completion.length; i++) {
+                        
+            String completeRunner = completion[i];
+            
+            // get -> decrease ->
+            int count = map.get(completeRunner);
+            
+            count--;
+        
+            if (count == 0) {
+                map.remove(completeRunner);
+                continue;
+            }
+            
+            map.put(completeRunner, count);
+            
+            
+        }
+        
+       return  map.keySet().stream().findFirst().orElse(null);
+        
     }
+}
